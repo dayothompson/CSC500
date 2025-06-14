8ae02b115e01ac543fe4bec019a2361fa0f2b620
Dayo Thompson <dayo.thompson@yahoo.com>
2025-06-14 17:01:48 -0400

    Commit_files

---
+++ Module1/part1.py
@@ -0,0 +1,22 @@
+# Part 1:
+# Write a Python program to find the addition and subtraction of two numbers.
+
+# Ask the user to input two numbers (num1 and num2). Given those two numbers, add them together to find the output. 
+# Also, subtract the two numbers to find the output.
+# Prompt the user to enter num1
+num1 = int(input("Enter num1: "))
+
+# Prompt the user to enter num2
+num2 = int(input("Enter num2: "))
+
+# Print separator
+print("--------------------------------")
+
+# Add the numbers
+print("Addition: ", num1 + num2)
+
+# Subtract the numbers
+print("Subtraction: ", num1 - num2)
+
+# Print separator
+print("--------------------------------")
---
+++ Module1/part2.py
@@ -0,0 +1,22 @@
+# Part 2:
+# Write a Python program to find the multiplication and division of two numbers.
+
+# Ask the user to input two numbers (num1 and num2). Given those two numbers, multiply them together to find the output. 
+# Also, divide num1/num2 to find the output.
+# Prompt the user to enter num1
+num1 = int(input("Enter num1: "))
+
+# Prompt the user to enter num2
+num2 = int(input("Enter num2: "))
+
+# Print separator
+print("--------------------------------")
+
+# Multiply the numbers
+print("Multiplication: ", num1 * num2)
+
+# Divide the numbers
+print("Division: ", num1 / num2)
+
+# Print separator
+print("--------------------------------")
